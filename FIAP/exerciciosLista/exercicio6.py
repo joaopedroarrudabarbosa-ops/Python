e38e4b23dc6d6f47a8fcbba5a6813811226c2651
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-04-28 17:44:47 -0300

    exercicios

--- FIAP/exerciciosLista/exercicio6.py
+++ FIAP/exerciciosLista/exercicio6.py
@@ -1,7 +1,20 @@
 lista = []
-notas = float(input("Digite umas nota ai: "))
+notas = float(input("Digite uma nota: "))
+
 while notas > 0:
     lista.append(notas)
+    notas = float(input("Digite uma nota: "))
+    cont = len(lista)
 print(f"Foram informadas {cont} notas.")
-print(lista)
+print(f"Notas informadas: {lista}.")
+
+media = sum(lista) / cont
+print(f"Media aritmedia foi: {media}.")
+
+acimaMedia = 0
+for i in lista:
+    if i > media:
+        acimaMedia += 1
+print(f"Quantidade de notas acima da média foram: {acimaMedia}.")
+
 
