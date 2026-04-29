f5265c9a5e303b0d5d5035633465a453aea088c6
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-04-29 12:13:19 -0300

    finalizado cap 5

---
+++ Python Development/capitulo5/documentar.py
@@ -0,0 +1,7 @@
+def soma(a:float,b:float):
+    """Função que realiza a soma de dois valores e retorna um resultado.
+    Dois argumentos, a e b, são organizados e são do tipo float."""
+    return a+b
+
+print(soma.__doc__)
+print(help(soma))
